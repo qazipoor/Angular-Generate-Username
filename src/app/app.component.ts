62dcac026d673dac4eb53ee96bec0a99b08018b4
Ali Qazipoor <qazipoor@gmail.com>
2022-08-25 20:09:42 +0300

    Objects and methods added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <form [formGroup]="form">
+      <label for="firstname">Enter your name</label>
+      <input type="text" formControlName="firstname" id="firstname" />
+      <label for="lastname">Enter your last name</label>
+      <input type="text" formControlName="lastname" id="lastname" />
+      <button (click)="generate()">Generate a user</button>
+      <p id="output">{{ userName | json }}</p>
+    </form>
+  `,
+  styles: [
+    'form { display: flex; flex-direction: column; }',
+    'input { margin-bottom: 10px; max-width: 150px; height: 20px; }',
+    'button { background-color: darkturquoise; border: 1px solid white; border-radius: 5px; max-width: 160px; width: 100%; height: 30px; margin-top: 10px; }'
+  ],
 })
 export class AppComponent {
   title = 'Angular-Generate-Username';
+
+  firstName: FormControl = new FormControl('Ali');
+  lastName: FormControl = new FormControl('Q');
+  userName!: string;
+
+  form = new FormGroup({
+    firstname: this.firstName,
+    lastname: this.lastName,
+  });
+
+  generate() {
+    this.userName =
+      this.form.value.firstname +
+      '_' +
+      this.form.value.lastname +
+      '_' +
+      (Math.floor(Math.random() * 9) + 1);
+  }
 }
